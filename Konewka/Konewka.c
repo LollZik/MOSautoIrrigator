dbb9979d0fcfe3db2102e679ddefb58934adfef9
LollZik <maciek2593@gmail.com>
2025-02-24 17:45:50 +0100

    Connection and sending data via UDP

--- Konewka/Konewka.c
+++ Konewka/Konewka.c
@@ -2,34 +2,34 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include "pico/cyw43_arch.h"
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+#include "lwip/ip_addr.h"
 
 
-//Pins
 #define MOISTURE_SENSOR_PIN 26
 #define VALVE_PIN 2
 
 
 #define MOISTURE_THRESHOLD 500 //0-4095
 
+//Network settings
 
-void setup(){
-    stdio_init_all();
-    
+#define WIFI_SSID "PicoNet"
+#define WIFI_PASSWORD "12345678"
+#define UDP_PORT 12345
+
+
+struct udp_pcb *udp_conn;
+ip_addr_t client_ip;
+uint16_t client_port = UDP_PORT;
+
+void setup_adc(){
     adc_init();
     adc_gpio_init(MOISTURE_SENSOR_PIN);
     adc_select_input(0);
-
-    gpio_init(VALVE_PIN);
-    gpio_set_dir(VALVE_PIN, GPIO_OUT);
-    gpio_put(VALVE_PIN,0); //Closed by default
 }
 
-void init_wifi(){
-    if(cyw43_arch_init()){
-        return;
-    }
-    cyw43_arch_deinit();
-}
 
 int read_moisture(){
     return adc_read();
@@ -44,12 +44,51 @@ void watering_controls(int moisture){
 
 }
 
+
+
+void setup_wifi(){
+    cyw43_arch_init();
+    cyw43_arch_enable_ap_mode(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK);
+}
+
+void setup_udp(){
+    udp_conn = udp_new();
+    ip_addr_set_any(IPADDR_TYPE_V4, &client_ip);
+    udp_bind(udp_conn, IPADDR_ANY, UDP_PORT);
+}
+
+void send_udp_data(int moisture, int valve_state){
+    char msg[32];
+    snprintf(msg, sizeof(msg),"Moisture:%d Valve:%d",moisture,valve_state);
+
+    struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, strlen(msg), PBUF_RAM);
+    if (!p) return;
+
+    memcpy(p -> payload, msg, strlen(msg));
+
+    udp_sendto(udp_conn, p, &client_ip, client_port);
+    pbuf_free(p);
+}
+
+
+
+
 int main(){
-    setup();
+    stdio_init_all();
+    setup_adc();
+    setup_wifi();
+    setup_udp();
+
+    gpio_init(VALVE_PIN);
+    gpio_set_dir(VALVE_PIN, GPIO_OUT);
+    gpio_put(VALVE_PIN,0); //Closed by default
 
     while (true) {
         int moisture = read_moisture();
+        int valve_state = gpio_get(VALVE_PIN);
+        send_udp_data(moisture, valve_state);
         watering_controls(moisture);
+
         sleep_ms(1800000); // 30 min
     }
 
---
+++ Wyswietlacz/Wyswietlacz.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include <hardware/spi.h>
+#include <pico/cyw43_arch.h>
+
+#define EINK_CS 17
+#define EINK_DC 16
+#define EINK_RST 15
+#define EINK_BUSY 14 
+
+#define WIFI_SSID "PicoNet"
+#define WIFI_PASS "12345678"
+#define UDP_PORT 12345
+
+struct udp_pcb *udp_conn;
+
+void eink_init(){
+    spi_init(spi0, 2 * 1000 * 1000);
+    gpio_set_function(18, GPIO_FUNC_SPI); // SCK
+    gpio_set_function(19, GPIO_FUNC_SPI); // MOSI
+    gpio_set_function(EINK_DC, GPIO_FUNC_SIO);
+    gpio_set_function(EINK_CS, GPIO_FUNC_SIO);
+    gpio_set_function(EINK_RST, GPIO_FUNC_SIO);
+    gpio_set_function(EINK_BUSY, GPIO_FUNC_SIO);
+
+    gpio_set_dir(EINK_DC, GPIO_OUT);
+    gpio_set_dir(EINK_CS, GPIO_OUT);
+    gpio_set_dir(EINK_RST, GPIO_OUT);
+    gpio_set_dir(EINK_BUSY, GPIO_IN);
+}
+
+void wifi_init(){
+    if(cyw43_arch_init()){
+        return;
+    }
+    cyw43_arch_enable_sta_mode();
+}
+
+void udp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t addr, u16_t port){
+    if (p != NULL){
+        char buf[32] = {0};
+        pbuf_copy_partial(p, buf, sizeof(buf)-1, 0);
+        pbuf_free(p);
+
+        int moisture = 0, valve_state = 0;
+        if (sscanf(buf, "Moisture:%d Valve:%d",&moisture, &valve_state) == 2){
+            printf("Moisture:%d Valve:%d",moisture, valve_state);
+            update_display(moisture, valve_state);
+        }
+    }
+}
+
+void setup_udp(){
+    udp_conn = udp_new();
+    if(!udp_conn){
+        return;
+    }
+    udp_bind(udp_conn, IP_ADDR_ANY, UDP_PORT);
+    udp_recv(udp_conn, udp_recv_callback, NULL);
+}
+
+
+void update_display(int moisture, int valve_status){\
+    char text[64];
+    snprintf(text, sizeof(text), "Moisture:%d\nValve: %d",moisture,valve_status ? "OPEN" : "CLOSED");
+    eink_clear();
+    eink_draw_text(text);
+    eink_refresh();
+}
+
+int main(){
+    stdio_init_all();
+    eink_init();
+    wifi_init();
+    setup_udp();
+
+    while (true){
+        receive_data();
+        update_display(600, 1);
+        sleep_ms(1800000);
+
+    }
+    return 0;
+}
+
