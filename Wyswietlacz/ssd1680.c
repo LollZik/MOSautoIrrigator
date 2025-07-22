4e2e96231bc0437867e9b1ca5a45cf322a54eff3
LollZik <maciek2593@gmail.com>
2025-07-22 15:36:24 +0200

    e-ink initialization fixed

--- Wyswietlacz/ssd1680.c
+++ Wyswietlacz/ssd1680.c
@@ -2,6 +2,8 @@
 #include "hardware/spi.h"
 #include "pico/stdlib.h"
 
+#define HEIGHT 122
+#define WIDTH 250
 
 void epd_reset(){
     gpio_put(EPD_RST,0);
@@ -38,36 +40,41 @@ void epd_init(){
     sleep_ms(10);
 
     epd_send_command(0x01);  // Driver output control
-    epd_send_data(0x27);     // Input direction
-    epd_send_data(0x01);     // display width
-    epd_send_data(0x00);     // display height
+    // Display's height
+    uint16_t m = HEIGHT - 1;
+    uint8_t mux_lsb =  m        & 0xFF;
+    uint8_t mux_msb = (m >> 8)  & 0x01;
+    epd_send_data(mux_lsb);
+    epd_send_data(mux_msb);
+    epd_send_data(0x00);     // Default input direction
 
     epd_send_command(0x11);  // Data entry mode
     epd_send_data(0x03);     // Y increment, X increment
-    epd_send_data(0x00);     // AM = "0" X-Mode
 
-
-//  Resolution
+//  Resolution (for 250x122 e-ink)
     epd_send_command(0x44);  // Set RAM X adress
     epd_send_data(0x00);     // XStart = 0
-    epd_send_data(0x3F);     // XEnd = 63
+    epd_send_data(0x1F);     // XEnd = 31 (width - 1 / 8)
 
     epd_send_command(0x45);  // Set RAM Y adress
     epd_send_data(0x00);     // YStart = 0
-    epd_send_data(0xFF);     // YEnd = 255
+    epd_send_data(0x00);     // 8th byte = 0
+    epd_send_data(0x79);     // YEnd = 121
+    epd_send_data(0x00);     // 8th byte = 0
 
     epd_send_command(0x3C);  // Border Waveform Control (Panel border)
-    epd_send_data(0x51);     // Fix level voltage
+    epd_send_data(0x01);     // simplest waveform
     
 //  Load waveform LUT
-    epd_send_command(0x18);  // Read from temperature register
-    epd_send_data(0x80);
+    epd_send_command(0x18);  // Temperature sensor control
+    epd_send_data(0x80);     // Use internal sensor
+    sleep_ms(2);             // Let the measurement stabilise
 
     epd_send_command(0x22);  // Display Update Control
     epd_send_data(0xB1);     // Load LUT from OTP
 
     epd_send_command(0x20);  // Execute Display Update Sequence
-    epd_wait_until_idle();
+    epd_wait_until_idle();   // Wait until finished
 }
 
 
